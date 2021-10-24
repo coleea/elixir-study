43f49febc9ad1cb3399284995aeea57356be58cb
lkb <tmwlak@gmail.com>
2021-10-24 17:44:32 +0900

    grammar added

---
+++ elixir_practice/hello_world.ex
@@ -0,0 +1,19 @@
+# 에러발생
+# defmodule 모듈 do
+#     def 메인 do
+#         name = IO.gets('이름?')
+#     end
+# end
+
+# 컴파일 에러
+# defmodule 모듈 do
+#     def main do
+#         name = IO.gets('이름?')
+#     end
+# end
+
+defmodule Modules do
+    def main do
+        name = IO.gets('이름?')
+    end
+end
